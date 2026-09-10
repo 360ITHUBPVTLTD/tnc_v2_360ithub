f7b799e83afb4c433f7769e3d210fd3e5055e464
Anurag <anurag.d@360ithub.com>
2026-09-10 13:01:43 +0530

    feat: Initialize App

---
+++ tnc_v2_360ithub/hooks.py
@@ -0,0 +1,255 @@
+app_name = "tnc_v2_360ithub"
+app_title = "TNC v2"
+app_publisher = "360ITHub"
+app_description = "TNC coaching institute ERP (v2 rebuild) on Frappe/ERPNext v15"
+app_email = "pankaj@360ithub.com"
+app_license = "mit"
+
+# Apps
+# ------------------
+
+# required_apps = []
+
+# Each item in the list will be shown as an app in the apps page
+# add_to_apps_screen = [
+# 	{
+# 		"name": "tnc_v2_360ithub",
+# 		"logo": "/assets/tnc_v2_360ithub/logo.png",
+# 		"title": "TNC v2",
+# 		"route": "/tnc_v2_360ithub",
+# 		"has_permission": "tnc_v2_360ithub.api.permission.has_app_permission"
+# 	}
+# ]
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/tnc_v2_360ithub/css/tnc_v2_360ithub.css"
+# app_include_js = "/assets/tnc_v2_360ithub/js/tnc_v2_360ithub.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/tnc_v2_360ithub/css/tnc_v2_360ithub.css"
+# web_include_js = "/assets/tnc_v2_360ithub/js/tnc_v2_360ithub.js"
+
+# include custom scss in every website theme (without file extension ".scss")
+# website_theme_scss = "tnc_v2_360ithub/public/scss/website"
+
+# include js, css files in header of web form
+# webform_include_js = {"doctype": "public/js/doctype.js"}
+# webform_include_css = {"doctype": "public/css/doctype.css"}
+
+# include js in page
+# page_js = {"page" : "public/js/file.js"}
+
+# include js in doctype views
+# doctype_js = {"doctype" : "public/js/doctype.js"}
+# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
+# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
+
+# Svg Icons
+# ------------------
+# include app icons in desk
+# app_include_icons = "tnc_v2_360ithub/public/icons.svg"
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+# 	"Role": "home_page"
+# }
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# Jinja
+# ----------
+
+# add methods and filters to jinja environment
+# jinja = {
+# 	"methods": "tnc_v2_360ithub.utils.jinja_methods",
+# 	"filters": "tnc_v2_360ithub.utils.jinja_filters"
+# }
+
+# Installation
+# ------------
+
+# before_install = "tnc_v2_360ithub.install.before_install"
+# after_install = "tnc_v2_360ithub.install.after_install"
+
+# Uninstallation
+# ------------
+
+# before_uninstall = "tnc_v2_360ithub.uninstall.before_uninstall"
+# after_uninstall = "tnc_v2_360ithub.uninstall.after_uninstall"
+
+# Integration Setup
+# ------------------
+# To set up dependencies/integrations with other apps
+# Name of the app being installed is passed as an argument
+
+# before_app_install = "tnc_v2_360ithub.utils.before_app_install"
+# after_app_install = "tnc_v2_360ithub.utils.after_app_install"
+
+# Integration Cleanup
+# -------------------
+# To clean up dependencies/integrations with other apps
+# Name of the app being uninstalled is passed as an argument
+
+# before_app_uninstall = "tnc_v2_360ithub.utils.before_app_uninstall"
+# after_app_uninstall = "tnc_v2_360ithub.utils.after_app_uninstall"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "tnc_v2_360ithub.notifications.get_notification_config"
+
+# Awesome Bar
+# -----------
+# Extra search results: list of dicts with label, description, route, index.
+# route: ["List", "ToDo"], "/desk/docs/some/page", or "https://example.com"
+# awesomebar_search = ["tnc_v2_360ithub.search.awesomebar_results"]
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# }
+
+# DocType Class
+# ---------------
+# Override standard doctype classes
+
+# override_doctype_class = {
+# 	"ToDo": "custom_app.overrides.CustomToDo"
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+# 	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"tnc_v2_360ithub.tasks.all"
+# 	],
+# 	"daily": [
+# 		"tnc_v2_360ithub.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"tnc_v2_360ithub.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"tnc_v2_360ithub.tasks.weekly"
+# 	],
+# 	"monthly": [
+# 		"tnc_v2_360ithub.tasks.monthly"
+# 	],
+# }
+
+# Testing
+# -------
+
+# before_tests = "tnc_v2_360ithub.install.before_tests"
+
+# Overriding Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "tnc_v2_360ithub.event.get_events"
+# }
+#
+# each overriding function accepts a `data` argument;
+# generated from the base implementation of the doctype dashboard,
+# along with any modifications made in other Frappe apps
+# override_doctype_dashboards = {
+# 	"Task": "tnc_v2_360ithub.task.get_dashboard_data"
+# }
+
+# exempt linked doctypes from being automatically cancelled
+#
+# auto_cancel_exempted_doctypes = ["Auto Repeat"]
+
+# Ignore links to specified DocTypes when deleting documents
+# -----------------------------------------------------------
+
+# ignore_links_on_delete = ["Communication", "ToDo"]
+
+# Request Events
+# ----------------
+# before_request = ["tnc_v2_360ithub.utils.before_request"]
+# after_request = ["tnc_v2_360ithub.utils.after_request"]
+
+# Job Events
+# ----------
+# before_job = ["tnc_v2_360ithub.utils.before_job"]
+# after_job = ["tnc_v2_360ithub.utils.after_job"]
+
+# User Data Protection
+# --------------------
+
+# user_data_fields = [
+# 	{
+# 		"doctype": "{doctype_1}",
+# 		"filter_by": "{filter_by}",
+# 		"redact_fields": ["{field_1}", "{field_2}"],
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_2}",
+# 		"filter_by": "{filter_by}",
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_3}",
+# 		"strict": False,
+# 	},
+# 	{
+# 		"doctype": "{doctype_4}"
+# 	}
+# ]
+
+# Authentication and authorization
+# --------------------------------
+
+# auth_hooks = [
+# 	"tnc_v2_360ithub.auth.validate"
+# ]
+
+# Automatically update python controller files with type annotations for this app.
+# export_python_type_annotations = True
+
+# default_log_clearing_doctypes = {
+# 	"Logging DocType Name": 30  # days to retain logs
+# }
+
+# Translation
+# ------------
+# List of apps whose translatable strings should be excluded from this app's translations.
+# ignore_translatable_strings_from = []
+
